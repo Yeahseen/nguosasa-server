f574ffe7e13b091d73223ecca1551c195f60fece
Yeahseen <51365179+Yeahseen@users.noreply.github.com>
2020-11-01 09:33:05 +0300

    working

--- nguosasa-app/src/Cart.js
+++ nguosasa-app/src/Cart.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import addToCart from './cartFunction';
 function Cart() {
   return (
-    <html>
-      <div className="mvls-container">
-        <div className="section-header">CART</div>
-        <div className="cart-row">
-          <span className="cart-item cart-header cart-column">ITEM</span>
-          <span className="cart-price cart-header cart-column">PRICE</span>
-          <span className="cart-quantity cart-header cart-column">
-            QUANTITY
-          </span>
-        </div>
-        <div className="cart-items"></div>
-
-        <div className="cart-total">
-          <strong className="cart-total-title">Total</strong>
-          <span className="cart-total-price">ksh 0</span>
-        </div>
-        <button className="btn btn-primary btn-purchase" type="button">
-          PURCHASE
-        </button>
+    <div className="mvls-container">
+      <div className="section-header">CART</div>
+      <div className="cart-row">
+        <span className="cart-item cart-header cart-column">ITEM</span>
+        <span className="cart-price cart-header cart-column">PRICE</span>
+        <span className="cart-quantity cart-header cart-column">QUANTITY</span>
       </div>
-    </html>
+      <div className="cart-items"></div>
+
+      <div className="cart-total">
+        <strong className="cart-total-title">Total</strong>
+        <span className="cart-total-price">ksh 0</span>
+      </div>
+      <button className="btn btn-primary btn-purchase" type="button">
+        PURCHASE
+      </button>
+    </div>
   );
 }
 
--- nguosasa-app/src/cartFunction.js
+++ nguosasa-app/src/cartFunction.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Cart from './Cart';
 
 //Onclick to cart
-
 function addToCartClicked(event) {
   var button = event.target;
   var shopItem = button.parentElement.parentElement.parentElement;
@@ -14,17 +13,26 @@ function addToCartClicked(event) {
 
   addItemToCart(title, price, imgSrc);
   function addItemToCart(name, price, poster) {
-    var cartItems = document.getElementsByClassName('cart-items')[0];
     var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+      if (cartItemNames[i].innerText === name) {
+        alert('This item is already added to the cart');
+        return;
+      }
+    }
     var cartRowContents = `
-  
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${poster}" width="100" height="100">
-            <span class="cart-item-title">${name}</span>
-            <span class="cart-price cart-column">${price}</span>
+<div class="cart-item cart-column">
+<img class="cart-item-image" src="${poster}" width="100" height="100">
+<span class="cart-item-title">${name}</span>
+</div>
+<span class="cart-price cart-column">${price}</span>
+<div class="cart-quantity cart-column">
 <input class="cart-quantity-input" type="number" value="1">
 <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`;
+</div>`;
 
     cartRow.innerHTML = cartRowContents;
     cartItems.append(cartRow);
@@ -38,7 +46,6 @@ function addToCartClicked(event) {
         button.addEventListener('click', addToCartClicked);
       }
     }
-    return Cart();
   }
   //adding to cart
 
